e13dc609533658f93671c9d5836095b7a1339841
Aaron Crosman <acrosman@gmail.com>
2021-05-29 14:53:11 -0400

    Fixing helper

---
+++ plugins/snowfakery_extras/snowHelper.py
@@ -0,0 +1,8 @@
+
+from snowfakery import SnowfakeryPlugin
+
+
+class SnowPunnary(SnowfakeryPlugin):
+    class Functions:
+        def snowPeter(self, word):
+            return 'Snow' + word + 'ary'
