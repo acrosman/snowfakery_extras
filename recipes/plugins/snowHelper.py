5009411d3525788b1ea6c86b8078fd09cf1c88f0
Aaron Crosman <acrosman@gmail.com>
2021-06-15 10:20:14 -0400

    Adding "ary" to the words to match the intended pun.

--- recipes/plugins/snowHelper.py
+++ recipes/plugins/snowHelper.py
@@ -5,4 +5,4 @@ from snowfakery import SnowfakeryPlugin
 class SnowPunnary(SnowfakeryPlugin):
     class Functions:
         def snowpunner(self, word):
-            return 'Snow' + word
+            return 'Snow' + word + 'ary'
