6011d0b5a1ea1558f13b00b60919e6f9799b8204
Aaron Crosman <acrosman@gmail.com>
2021-10-30 18:28:39 -0400

    Adding counting example

--- recipes/plugins/snowHelper.py
+++ recipes/plugins/snowHelper.py
@@ -6,3 +6,9 @@ class SnowPunnary(SnowfakeryPlugin):
     class Functions:
         def snowpunner(self, word):
             return 'Snow' + word + 'ary'
+
+        def currentCounter(self):
+            context_vars = self.context.context_vars()
+            context_vars.setdefault("count", 0)
+            context_vars["count"] += 1
+            return context_vars["count"]
